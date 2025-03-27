a99802f996243dec2f0c93db0a4acf1f3eb47ac6
ArianG421 <Arianz421@gmail.com>
2025-03-27 13:19:49 +0100

    Initial Commit

---
+++ src/main/java/com/example/tourismroullete/TourismroulleteApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourismroullete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismroulleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourismroulleteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tourismroullete/TourismroulleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tourismroullete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismroulleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
