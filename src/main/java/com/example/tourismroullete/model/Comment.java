10a91237dc003978f3f8275012e785ea31282f4c
Abdifatah Abdulkadir <abdifatah.abdulkadir@iths.se>
2025-04-03 14:59:53 +0200

    Add Lombok annotations to Comment entity - Added @Data for getters/setters - Added @Builder for builder pattern - Added @NoArgsConstructor and @AllArgsConstructor - Improved code structure with Lombok

--- src/main/java/com/example/tourismroullete/model/Comment.java
+++ src/main/java/com/example/tourismroullete/model/Comment.java
@@ -1,11 +1,17 @@
 package com.example.tourismroullete.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
