e368ee2f5b9944fe4b442173b66c045c93e09c01
Abdifatah Abdulkadir <abdifatah.abdulkadir@iths.se>
2025-04-10 11:12:01 +0200

    Implemented like/unlike functionality in LikeRepository

--- src/main/java/com/example/tourismroullete/repository/LikeRepository.java
+++ src/main/java/com/example/tourismroullete/repository/LikeRepository.java
@@ -1,4 +1,12 @@
 package com.example.tourismroullete.repository;
 
-public class LikeRepository {
+import com.example.tourismroullete.model.Like;
+import com.example.tourismroullete.model.Post;
+import com.example.tourismroullete.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// LikeRepository.java - Fixa detta!
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    boolean existsByPostAndUser(Post post, User user);
+    void deleteByPostAndUser(Post post, User user);
 }
